081b283e76d5201df8694213cdf551e129da4d44
Phypix96 <philipp.haim@gmail.com>
2017-05-31 19:28:22 +0200

    Bug in compression solved
    Matrices were compressed to the wrong dimensions and summation over one index was missing

--- Julia/compress.jl
+++ Julia/compress.jl
@@ -1,16 +1,15 @@
+using Gallium
+
 function compress(C, D::Int64=Inf; direction="R")
   L=size(C)[1];
-  norm=0.;
   if direction=="L"
     for i=1:L-1
       (A,M)=left_norm(C[i], C[i+1], D);
       C[i]=A;
       C[i+1]=M;
     end
-    for σ=1:size(C[1])[3]
-      norm+=*(C[L][:,:,σ]',C[L][:,:,σ])
-    end
-    #C[L]=C[L]/sqrt(abs(norm[1]))
+    A=left_norm(C[L],D)
+    C[L]=A
 
   elseif direction=="R"
     for i=0:L-2
@@ -18,15 +17,46 @@ function compress(C, D::Int64=Inf; direction="R")
       C[L-i]=B;
       C[L-i-1]=M;
     end
-
-    for σ=1:size(C[1])[3]
-      norm+=*(C[1][:,:,σ],C[1][:,:,σ]')
-    end
-    #C[1]=C[1]/sqrt(abs(norm[1]))
+    B=right_norm(C[1],D)
+    C[1]=B
   end
 end
 
+
+
 function left_norm(A::Array{Complex128}, M::Array{Complex128}, D::Int64=Inf)
+  (D1,D2,d)=size(A);
+  (D3,D4,d)=size(M);
+  H=complex(zeros(D1*d, D2));
+  #grouping σ-matricies atop each other for svd
+  for i = 1:D1 j=1:D2
+    for σ =1:d
+      H[σ*D1+i-D1,j]=A[i,j,σ];
+    end
+  end
+  (U,S,V)=svd(H);
+
+  D_is=Int64(min(D,size(S)[1]))  #if size has to be truncated, match the matrix dimensions
+  M2=complex(zeros(D_is,D4,d))
+
+  for σ = 1:d
+    for i=1:D1 j = 1:D_is
+      A[i,j,σ]=U[σ*D1+i-D1,j]; #splitting U-maticies at [σ*D1+i-D1,j] into A-matricies again
+    end
+    #M[k,j,σ]=*(diagm(S[k]),transpose(V[k,i]),M[i,j,σ]);
+    for i=1:D3, j=1:D4
+      for k=1:D_is
+        M2[k,j,σ]+=S[k]*V'[k,i]*M[i,j,σ]; #new matrix left of the compressed
+      end
+    end
+  end
+
+  A=A[:,1:D_is,:]
+
+  return (A,M2)
+end
+
+function left_norm(A::Array{Complex128}, D::Int64=Inf)
   (D1,D2,d)=size(A);
   H=complex(zeros(D1*d, D2));
   #grouping σ-matricies atop each other for svd
@@ -42,15 +72,43 @@ function left_norm(A::Array{Complex128}, M::Array{Complex128}, D::Int64=Inf)
     for i=1:D1 j = 1:D_is
       A[i,j,σ]=U[σ*D1+i-D1,j]; #splitting V^†-maticies at [i,σ*D2+j-D2] into A-matricies again
     end
-    M[1:D_is,:,σ]=*(diagm(S[1:D_is]),transpose(V[:,1:D_is]),M[:,:,σ]); #new matrix left of the compressed
   end
 
-  A=A[:,1:min(D,D2),:]
-  M=M[1:min(D,size(M)[1]),:,:]
-  return (A,M)
+  A=A[:,1:D_is,:]
+  return (A)
 end
 
+
+
 function right_norm(B::Array{Complex128}, M::Array{Complex128}, D::Int64=Inf)
+  (D1,D2,d)=size(B);
+  (D3,D4,d)=size(M)
+  H=complex(zeros(D1, D2*d));  #grouping σ-matricies next to each other for svd
+  for j = 1:D2 i=1:D1
+    for σ =1:d
+      H[i,σ*D2+j-D2]=B[i,j,σ];
+    end
+  end
+  (U,S,V)=svd(H);
+
+  D_is=Int64(min(D,size(S)[1]))  #if size has to be truncated, match the matrix dimensions
+  B2=complex(zeros(D_is,D2,d))
+  M2=complex(zeros(D3,D_is,d))
+  for σ = 1:d
+    for i=1:D_is j = 1:D2
+      B2[i,j,σ]=V[σ*D2+j-D2,i]; #splitting V^†-maticies at [i,σ*D2+j-D2] into B-matricies again
+    end
+    for i=1:D3, j=1:D4
+      for k=1:D_is
+        M2[i,k,σ]+=M[i,j,σ]*U[j,k]*S[k]; #new matrix left of the compressed
+      end
+    end
+    #[:,1:D_is,σ]=*(M[:,:,σ],U[:,1:D_is],diagm(S[1:D_is])); #new matrix left of the compressed
+  end
+  return (B2,M2)
+end
+
+function right_norm(B::Array{Complex128}, D::Int64=Inf)
   (D1,D2,d)=size(B);
   H=complex(zeros(D1, D2*d));  #grouping σ-matricies next to each other for svd
   for j = 1:D2 σ =1:d
@@ -63,9 +121,9 @@ function right_norm(B::Array{Complex128}, M::Array{Complex128}, D::Int64=Inf)
     for i=1:D_is j = 1:D2
       B[i,j,σ]=V[σ*D2+j-D2,i]; #splitting V^†-maticies at [i,σ*D2+j-D2] into B-matricies again
     end
-    M[:,1:D_is,σ]=*(M[:,:,σ],U[:,1:D_is],diagm(S[1:D_is])); #new matrix left of the compressed
   end
-  B=B[1:min(D,D1),:,:]
-  M=M[:,1:min(D,size(M)[2]),:]
-  return (B,M)
+  B=B[1:D_is,:,:]
+
+  return (B)
 end
+#breakpoint(left_norm)
